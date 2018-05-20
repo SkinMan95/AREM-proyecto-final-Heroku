53d4270914f1470d6f1fc1b6066a769f7d7786f1
Alejandro Anzola <anzola95avila@gmail.com>
2018-05-20 11:50:08 -0500

    ajustes para servir pagina web

--- src/main/java/edu/escuelaing/arem/squarenumber/SquareNumberController.java
+++ src/main/java/edu/escuelaing/arem/squarenumber/SquareNumberController.java
@@ -1,10 +1,6 @@
 package edu.escuelaing.arem.squarenumber;
 
-import java.math.BigInteger;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,12 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Alejandro Anzola email: alejandro.anzola@mail.escuelaing.edu.co
  */
 @RestController
-@RequestMapping(value = "/square")
+@RequestMapping(value = "/api")
 public class SquareNumberController {
-
-    @GetMapping
-    public @ResponseBody
-    String squareNumber(@RequestParam("value") BigInteger value) {
-        return value.multiply(value).toString();
-    }
+    
 }
